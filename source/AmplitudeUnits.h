8b2229c7bbdbbc0dcd792433467c6c2647817d48
Sinecure Audio <contact@SinecureAudio.com>
2019-11-21 11:13:38 +0100

    Removed unnecessary semicolons after functions

--- source/AmplitudeUnits.h
+++ source/AmplitudeUnits.h
@@ -17,15 +17,15 @@ class Amplitude : public Unit<Amplitude, InputType>
 {
 public:
 	constexpr Amplitude() = default;
-	constexpr Amplitude(const InputType& initValue) { this->value = initValue; };
+	constexpr Amplitude(const InputType& initValue) { this->value = initValue; }
 
 	template <typename T, std::enable_if_t<std::is_convertible_v<InputType, T>>>
-	constexpr Amplitude(const Amplitude<T>& other) { this->value = InputType(other.count()); };
+	constexpr Amplitude(const Amplitude<T>& other) { this->value = InputType(other.count()); }
 
     template<typename T>
 	constexpr Amplitude(const Decibel<T>& decibelValue)  { this->value = convertDecibelToAmplitude(decibelValue.count()); }
     template<typename T>
-	Amplitude& operator=(const Decibel<T>& decibelValue) { this->value = convertDecibelToAmplitude(decibelValue.count()); return *this; };
+	Amplitude& operator=(const Decibel<T>& decibelValue) { this->value = convertDecibelToAmplitude(decibelValue.count()); return *this; }
 
 	static constexpr InputType convertDecibelToAmplitude(const InputType& decibelValue, const InputType& minusInfinityDb = defaultMinusInfinitydB) {
 		return decibelValue > minusInfinityDb
@@ -44,11 +44,11 @@ public:
 	constexpr Decibel() = default;
 
 	template <typename T> //, std::enable_if_t<std::is_convertible_v<InputType, T>>>
-	constexpr Decibel(const Decibel<T>& other) {this->value = InputType(other.count());};
-	constexpr explicit Decibel(const InputType& initValue) { this->value = initValue; };
+	constexpr Decibel(const Decibel<T>& other) {this->value = InputType(other.count());}
+	constexpr explicit Decibel(const InputType& initValue) { this->value = initValue; }
 	constexpr Decibel(const Amplitude<InputType>& amplitudeValue) {this->value = convertAmplitudeToDecibel(amplitudeValue.count());}
 
-	Decibel& operator=(const Amplitude<InputType>& amplitudeValue)  { this->value = convertAmplitudeToDecibel(amplitudeValue.count()); return *this; };
+	Decibel& operator=(const Amplitude<InputType>& amplitudeValue)  { this->value = convertAmplitudeToDecibel(amplitudeValue.count()); return *this; }
 
 	static constexpr auto convertAmplitudeToDecibel(const InputType& amplitudeValue, const InputType& minusInfinitydB = defaultMinusInfinitydB) {
 		return std::max(minusInfinitydB, std::log10(amplitudeValue)* InputType{20});
@@ -116,8 +116,8 @@ std::ostream& operator<<(std::ostream& out, const  Decibel<T>& decibel) {
 //user defined litereals for making decibel templates
 constexpr auto operator"" _dB(long double val) {
 	return Decibel<long double>(val);
-};
+}
 
 constexpr auto operator"" _dB(unsigned long long val) {
 	return Decibel<unsigned long long >(val);
-};
+}
--- source/FilterUnits.h
+++ source/FilterUnits.h
@@ -14,9 +14,9 @@ public:
 	constexpr explicit ResonanceCoefficient(const InputType& initValue) {this->value = initValue;}
 
 	template <typename T>//	, std::enable_if_t<std::is_convertible_v<InputType, T>>>
-	constexpr ResonanceCoefficient(const ResonanceCoefficient<T>& other) { this->value = InputType(other.count()); };
+	constexpr ResonanceCoefficient(const ResonanceCoefficient<T>& other) { this->value = InputType(other.count()); }
 
-	ResonanceCoefficient& operator=(const QCoefficient<InputType>&& newCoefficient) noexcept { this->value = convertQToResonance(newCoefficient.count()); return *this; };
+	ResonanceCoefficient& operator=(const QCoefficient<InputType>&& newCoefficient) noexcept { this->value = convertQToResonance(newCoefficient.count()); return *this; }
 
 	static InputType convertQToResonance(const InputType& QCoefficient) {
 		return InputType{1.0} - InputType{1.0} / (InputType{2.0}*QCoefficient);
@@ -32,11 +32,11 @@ public:
 	constexpr explicit QCoefficient(const InputType& initValue) {this->value = initValue;}
 
 	template <typename T, std::enable_if_t<std::is_convertible_v<InputType, T>>>
-	constexpr QCoefficient(const QCoefficient<T>& other) { this->value = InputType(other.count()); };
+	constexpr QCoefficient(const QCoefficient<T>& other) { this->value = InputType(other.count()); }
 
-	constexpr QCoefficient(const ResonanceCoefficient<InputType>& resonanceValue)   { this->value = convertResonanceToQ (resonanceValue .count()); };
+	constexpr QCoefficient(const ResonanceCoefficient<InputType>& resonanceValue)   { this->value = convertResonanceToQ (resonanceValue .count()); }
 
-	QCoefficient& operator=(const ResonanceCoefficient<InputType>& newCoefficient)  noexcept { this->value = convertResonanceToQ(newCoefficient .count()); return *this; };
+	QCoefficient& operator=(const ResonanceCoefficient<InputType>& newCoefficient)  noexcept { this->value = convertResonanceToQ(newCoefficient .count()); return *this; }
 
 	static InputType convertResonanceToQ(const InputType& resonanceCoefficient) {
 		return static_cast<InputType>(1.0 / (2.0*(1.0 - resonanceCoefficient)));
